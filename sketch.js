2f60fa61a9ecdc9ac38e9072019c1cd06a202cab
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-01-16 21:16:39 -0600

    Traducción 1.0

--- sketch.js
+++ sketch.js
@@ -29,13 +29,13 @@ function setup() {
   
   createCanvas(windowWidth,windowHeight);
 
-  //adding the background image
+  // Agregando la imagen de fondo
   bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
 bg.addImage(bgImg)
 bg.scale = 1.1
   
 
-//creating the player sprite
+// Creando el sprite del jugador
 player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
  player.addImage(shooterImg)
    player.scale = 0.3
@@ -43,7 +43,7 @@ player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
    player.setCollider("rectangle",0,0,300,300)
 
 
-   //creating sprites to depict lives remaining
+   // Creando sprites para representar la vida restante
    heart1 = createSprite(displayWidth-150,40,20,20)
    heart1.visible = false
     heart1.addImage("heart1",heart1Img)
@@ -59,14 +59,14 @@ player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
     heart3.scale = 0.4
    
 
-    //creating group for zombies    
+    // Creando un grupo para zombis
     zombieGroup = new Group();
 }
 
 function draw() {
   background(0); 
 
-  //moving the player up and down and making the game mobile compatible using touches
+  // Moviendo al jugador arriba y abajo y haciendo al juego compatible con dispositivos móviles a través de entrada táctil
 if(keyDown("UP_ARROW")||touches.length>0){
   player.y = player.y-30
 }
@@ -75,7 +75,7 @@ if(keyDown("DOWN_ARROW")||touches.length>0){
 }
 
 
-//release bullets and change the image of shooter to shooting position when space is pressed
+// Liberar balas y cambiar la imagen del tirador a posición de disparo cuando se presiona la barra espaciadora
 if(keyWentDown("space")){
   
   player.addImage(shooter_shooting)
@@ -83,13 +83,13 @@ if(keyWentDown("space")){
  
 }
 
-//player goes back to original standing image once we stop pressing the space bar
+// El jugador regresa a la posición original de pie una vez que se deja de precionar la barra espaciadora
 else if(keyWentUp("space")){
   player.addImage(shooterImg)
 }
 
 
-//destroy zombie when player touches it
+// Destruye al zombi una vez que el jugador lo toca
 if(zombieGroup.isTouching(player)){
  
 
@@ -101,7 +101,7 @@ if(zombieGroup.isTouching(player)){
  }
 }
 
-//calling the function to spawn zombies
+// Llamando a la función para generar zombis
 enemy();
 
 drawSprites();
@@ -109,11 +109,11 @@ drawSprites();
 
 
 
-//creating function to spawn zombies
+// Creando la función para generar zombis
 function enemy(){
   if(frameCount%50===0){
 
-    //giving random x and y positions for zombie to appear
+    // Dando posiciónes X y Y aleatorias para que aparezca el zombi
     zombie = createSprite(500,500,40,40)
 
     zombie.addImage(zombieImg)
